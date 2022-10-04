e5a16c49c58236371c76db269a4f8466bab72e9f
Trent Baldwin <tabbcb63@gmail.com>
2022-10-04 12:24:50 -0500

    Refactored with hug framework

--- src/receiver.py
+++ src/receiver.py
@@ -1,23 +1,9 @@
 """Receives incoming game logs and passes them to
 the correct interface"""
 
-import http.server
-import socketserver
+import hug
 
-class learning_http_handler(http.server.BaseHTTPRequestHandler):
-    def do_POST(self):
-        length = int(self.headers.get("content-length"))
-        data = self.rfile.read(length)
-        self.temp_str = data.decode()
-        self.send_response(200)
-        self.end_headers()
-        return self.temp_str
-
-if __name__ == "__main__":
-    PORT = 8000
-    IP = "localhost"
-    HANDLER = learning_http_handler
-
-    with socketserver.TCPServer((IP, PORT), HANDLER) as httpd:
-        print(f"Serving at ip {IP} and port {PORT}")
-        httpd.serve_forever()
+@hug.post("/upload")
+def upload_file(body):
+    print(body)
+    return body
--- test_receiver.py
+++ test_receiver.py
@@ -1,45 +1,8 @@
-from threading import Thread
-import requests
-import socketserver
-import time
-import socket
+import hug
 import src.receiver as receiver
 
-
-def send_post_request(IP,PORT, result, TCP_server=None):
-    time.sleep(5)
-    result.append(requests.post(f"http://{IP}:{str(PORT)}/", data="TEST", timeout=10))
-    if TCP_server:
-        TCP_server.shutdown()
-
-def test_http_post_response():
-    PORT = 8000
-    IP = "localhost"
-    HANDLER = receiver.learning_http_handler
-
-    with socketserver.TCPServer((IP, PORT), HANDLER) as httpd:
-        print(f"Serving at ip {IP} and port {PORT}")
-        response = []
-        t = Thread(target=send_post_request, args=(IP, PORT, response, httpd,))
-        t.start()
-        httpd.serve_forever()
-        t.join()
-
-        assert response[0].status_code == 200
-
-def test_http_post_capture():
-    # TEST_STR = b"Hello"
-    # CLIENT = ("192.168.1.1", 8080)
-    # SERVER = ("localhost", 8080)
-    # SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # SOCK.bind(SERVER)
-    # t = Thread(target=send_post_request, args=(SERVER[0], SERVER[0], [],))
-    # t.start()
-    # SOCK.listen(2)
-    # SOCKET, addr = SOCK.accept()
-    # t.join()
-
-    # handler = receiver.learning_http_handler(SOCKET, CLIENT, SERVER)
-    # #handler.headers.add_header("content-length", len(TEST_STR))
-    # data = handler.do_POST()
-    # assert data == TEST_STR
+def test_upload_file():
+    TEST_STR = b"JSON STRING"
+    response = hug.test.post(receiver, "/upload", TEST_STR)
+    assert response.status == hug.HTTP_200
+    assert response.data == TEST_STR.decode("utf-8")
