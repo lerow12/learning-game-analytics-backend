84ad0b24ac994e6e54a1a7796caaa4902b411352
Timothy Ross <tbross1@crimson.ua.edu>
2022-10-11 23:38:26 -0500

    Fixed errors.

--- src/database.py
+++ src/database.py
@@ -2,16 +2,16 @@ import imp
 import mysql.connector
 import database_strings as dbs
 """Repackages events and stores them in mysql database"""
+#Temporary Credentials. Replace with your own to test.
 database_name = "NyingiDatabase"
 host = "localhost"
 user = "test"
 password = "password"
-initflag = False
 
 def deleteDatabase(host, user, password, database_name):
     init_database = mysql.connector.connect(host=host, user=user, password=password)
     init_db_cursor = init_database.cursor()
-    init_db_cursor.execute("DROP DATABASE [IF EXISTS] {};".format(database_name))
+    init_db_cursor.execute("DROP DATABASE IF EXISTS {};".format(database_name))
     init_db_cursor.close()
 
 
@@ -19,20 +19,16 @@ def createDatabase(host, user, password, database_name):
     init_database = mysql.connector.connect(host=host, user=user, password=password)
     init_db_cursor = init_database.cursor()
     init_db_cursor.execute("CREATE DATABASE {};".format(database_name))
-    initflag = True
     init_db_cursor.close()
     createTables(host, user, password, database_name)
     
 def createTables(host, user, password, database_name):
-    if not initflag:
-        print("Initialize the database before use.")
-        return
     init_database = mysql.connector.connect(host=host, user=user, password=password, database=database_name)
     init_db_cursor = init_database.cursor()
     query_list = [dbs.Create_BoardDif, dbs.Create_CardDif, dbs.Create_GameTable, dbs.Create_Player_Events]
     for query in query_list:
         init_db_cursor.execute(query)
 
-createDatabase(host, user, password, database_name)
-deleteDatabase(host, user, password, database_name)
+#createDatabase(host, user, password, database_name)
+#deleteDatabase(host, user, password, database_name)
 
