ca39fd16bb1d46f42e196b5c7d4e8818232efe97
Timothy Ross <tbross1@crimson.ua.edu>
2022-10-13 17:44:03 -0500

    Slight edits to functions and SQL tables.

--- src/database.py
+++ src/database.py
@@ -6,6 +6,11 @@ database_name = "NyingiDatabase"
 host = "localhost"
 user = "test"
 password = "password"
+nyingi_query_list = [
+        dbs.Create_BoardDif,
+        dbs.Create_CardDif,
+        dbs.Create_GameTable,
+        dbs.Create_Player_Events]
 
 
 def deleteDatabase(host, user, password, database_name):
@@ -19,7 +24,7 @@ def deleteDatabase(host, user, password, database_name):
     init_db_cursor.close()
 
 
-def createDatabase(host, user, password, database_name):
+def createDatabase(host, user, password, database_name, query_list):
     init_database = mysql.connector.connect(
         host=host,
         user=user,
@@ -27,23 +32,15 @@ def createDatabase(host, user, password, database_name):
     init_db_cursor = init_database.cursor()
     init_db_cursor.execute("CREATE DATABASE {};".format(database_name))
     init_db_cursor.close()
-    createTables(host, user, password, database_name)
+    createTables(host, user, password, database_name, query_list)
 
 
-def createTables(host, user, password, database_name):
+def createTables(host, user, password, database_name, query_list):
     init_database = mysql.connector.connect(
         host=host,
         user=user,
         password=password,
         database=database_name)
     init_db_cursor = init_database.cursor()
-    query_list = [
-        dbs.Create_BoardDif,
-        dbs.Create_CardDif,
-        dbs.Create_GameTable,
-        dbs.Create_Player_Events]
     for query in query_list:
         init_db_cursor.execute(query)
-
-# createDatabase(host, user, password, database_name)
-# deleteDatabase(host, user, password, database_name)
--- src/database_strings.py
+++ src/database_strings.py
@@ -2,25 +2,28 @@
 Create_Player_Events = "CREATE TABLE PlayerEvents\
 (event_id INT AUTO_INCREMENT PRIMARY KEY, \
 player_num INT NOT NULL, player_id INT, \
-is_swap BIT NOT NULL, is_game_over BIT DEFAULT 0, \
-timestamp TIMESTAMP, card_diff_id INT, board_diff_id INT, game_id INT, \
+is_swap BIT NOT NULL, is_successful BIT, \
+timestamp TIMESTAMP, play_time TIME, \
+card_diff_id INT, board_diff_id INT, game_id INT, \
 FOREIGN KEY (card_diff_id) REFERENCES CardDiff(card_diff_id), \
 FOREIGN KEY (board_diff_id) REFERENCES BoardDiff(board_diff_id), \
 FOREIGN KEY (game_id) REFERENCES GameTable(game_id));"
 
+# hand is hand before play
 Create_CardDif = "CREATE TABLE CardDiff\
 (card_diff_id INT AUTO_INCREMENT PRIMARY KEY, \
-hand VARCHAR(7), cards_lost VARCHAR(7), \
+hand VARCHAR(7), cards_used VARCHAR(7), \
 cards_gained VARCHAR(7), player_num INT);"
 
+# taken_matrix is taken state before play.
 Create_BoardDif = "CREATE TABLE BoardDiff\
 (board_diff_id INT AUTO_INCREMENT PRIMARY KEY, \
-x INT, y INT, val INT, board_state VARBINARY(256), \
+x INT, y INT, val INT, \
 taken_matrix VARBINARY(256), player_num INT);"
 
 Create_GameTable = "CREATE TABLE GameTable\
 (game_id INT AUTO_INCREMENT PRIMARY KEY, \
 board_area INT, max_value INT, computer_difficulty VARCHAR(10), \
-game_name VARCHAR(32), board_state VARBINARY(256), \
+game_name VARCHAR(32), board_state VARBINARY(256), winner INT, \
 computer_hand VARBINARY(7), \
 player_hand VARBINARY(7));"
