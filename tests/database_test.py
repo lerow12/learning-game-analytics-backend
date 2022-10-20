722fd850c91c77f0efb1de97d3755c24894661cc
Timothy Ross <tbross1@crimson.ua.edu>
2022-10-19 20:49:16 -0500

    removed test.

--- tests/database_test.py
+++
@@ -1,6 +0,0 @@
-from src.database import *
-
-
-def test_create_database():
-    create_database(host, user, password, database_name, nyingi_query_list)
-    delete_database(host, user, password, database_name)
