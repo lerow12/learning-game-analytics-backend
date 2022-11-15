a1591e7dc0a8bed9b4d1b2e05bfcf04678ef1f38
Trent Baldwin <tbaldwin4@crimson.ua.edu>
2022-11-15 00:16:40 -0600

    Standard table output finished

--- src/visualize.py
+++ src/visualize.py
@@ -9,6 +9,12 @@ host = "localhost"
 user = "test"
 password = "password"
 
+def print_row(max_width, row):
+    print("| ", end="")
+    width = max_width
+    for arg in row:
+        print(f"{arg:^{width}} | ", end="")
+    print()
 
 """Visualization tool"""
 
@@ -24,27 +30,36 @@ args = parser.parse_args()
 
 # Attempt to run SQL Querry
 try:
-  cnx = mysql.connector.connect(
+    cnx = mysql.connector.connect(
         host=host,
         user=user,
         password=password,
         database=database_name)
-  db_cursor = cnx.cursor()
-  db_cursor.execute(args.querry)
-  for t in db_cursor:
-    print(t)
-  db_cursor.close()
+    db_cursor = cnx.cursor()
+    db_cursor.execute(args.querry)
+    result = db_cursor.fetchall()
+
+    # Get the length of the longest cell in a row
+    max_width = 0
+    for row in result:
+        max_width = max(max_width, max(list(map(len, (map(str, row))))))
+
+    # Format print each row
+    for row in result:
+        print_row(max_width, row)
+
+    db_cursor.close()
 
 # Catch DB Exeptions
 except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
 
 # Close DB
 else:
-  cnx.close()
+    cnx.close()
 
