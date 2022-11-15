5a580eaa45cda7c5f8c587b0b498da7d7763d3d7
Timothy Ross <tbross1@crimson.ua.edu>
2022-11-15 16:09:25 -0600

    Fixed injection issues with prepared statements.

--- src/database_calls.py
+++ src/database_calls.py
@@ -31,14 +31,15 @@ def save_game_table(board_area, max_value, computer_difficulty, game_name, board
         password=password,
         database=database_name
     )
-    cur = con.cursor()
+    cur = con.cursor(prepared=True)
     board_state = delimit(board_state)
     computer_hand = delimit(computer_hand)
     player_hand = delimit(player_hand)
+    args_tuple = (board_area, max_value, computer_difficulty, game_name, board_state, winner, computer_hand, player_hand)
     cur.execute(f"""
         INSERT INTO GameTable(board_area, max_value, computer_difficulty, game_name, board_state, winner, computer_hand, player_hand)
-        VALUES({board_area}, {max_value}, '{computer_difficulty}', '{game_name}', '{board_state}', {winner}, '{computer_hand}', '{player_hand}');
-    """)
+        VALUES(%s, %s, %s, %s, %s, %s, %s, %s);
+    """, args_tuple)
     con.commit()
     game_id = cur.lastrowid
     con.close()
@@ -52,12 +53,13 @@ def save_board_diff(x, y, taken_matrix, player_num):
         password=password,
         database=database_name
     )
-    cur = con.cursor()
+    cur = con.cursor(prepared=True)
     taken_matrix = list_to_varbinary(taken_matrix)
-    cur.execute(f"""
+    args_tuple = (x, y, taken_matrix, player_num)
+    cur.execute("""
         INSERT INTO BoardDiff(x, y, taken_matrix, player_num)
-        VALUES({x}, {y}, '{taken_matrix}', {player_num});
-    """)
+        VALUES(%s, %s, %s, %s);
+    """, args_tuple)
     con.commit()
     diff_id = cur.lastrowid
     con.close()
@@ -71,14 +73,15 @@ def save_card_diff(hand, cards_used, cards_gained, player_num):
         password=password,
         database=database_name
     )
-    cur = con.cursor()
+    cur = con.cursor(prepared=True)
     hand = delimit(hand)
     cards_used = delimit(cards_used)
     cards_gained = delimit(cards_gained)
-    cur.execute(f"""
+    arg_tuple = (hand, cards_used, cards_gained, player_num)
+    cur.execute("""
         INSERT INTO CardDiff(hand, cards_used, cards_gained, player_num)
-        VALUES('{hand}', '{cards_used}', '{cards_gained}', {player_num});
-    """)
+        VALUES(%s, %s, %s, %s);
+    """, arg_tuple)
     con.commit()
     diff_id = cur.lastrowid
     con.close()
@@ -92,18 +95,19 @@ def save_player_events(player_num, player_id, is_swap, is_successful, timestamp,
         password=password,
         database=database_name
     )
-    cur = con.cursor()
+    cur = con.cursor(prepared=True)
     if (card_diff_id == 0):
-        card_diff_id = "NULL"
+        card_diff_id = None
     if (board_diff_id == 0):
-        board_diff_id = "NULL"
+        board_diff_id = None
     if (inputs == []):
-        inputs = "NULL"
+        inputs = None
     else:
         inputs = delimit(inputs)
-    cur.execute(f"""
+    arg_tuple = (player_num, player_id, is_swap, is_successful, timestamp, play_time, inputs, card_diff_id, board_diff_id, game_id)
+    cur.execute("""
         INSERT INTO PlayerEvents(player_num, player_id, is_swap, is_successful, timestamp, play_time, inputs, card_diff_id, board_diff_id, game_id)
-        VALUES({player_num}, {player_id}, {is_swap}, {is_successful}, '{timestamp}', '{play_time}', '{inputs}', {card_diff_id}, {board_diff_id}, {game_id});
-    """)
+        VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+    """, arg_tuple)  
     con.commit()
     con.close()
