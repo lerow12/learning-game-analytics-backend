b2f65d2cac982dffd929997b909b5fb9f660cd3f
Trent Baldwin <tbaldwin4@crimson.ua.edu>
2022-11-15 10:13:36 -0600

    Shrink table to more reasonable size

--- src/visualize.py
+++ src/visualize.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from mysql.connector import errorcode
 import argparse
+import datetime
 
 
 # Temp Creds
@@ -23,14 +24,18 @@ parser.add_argument("-q", "--querry", metavar = "SQL Querry", required = True, h
 args = parser.parse_args()
 
 
-def print_row(max_width, row, header=False):
+def print_row(max_widths, row, header=False):
     """Prints a database table row with each cell the max_width size"""
     print("| ", end="")
-    for arg in row:
-        print(f"{arg:^{max_width}} | ", end="")
+    for index, arg in enumerate(row):
+        if type(arg) == bytearray:
+            arg = arg.decode()
+        if type(arg) != str:
+            arg = str(arg)
+        print(f"{arg:^{max_widths[index]}} | ", end="")
     print()
     if header:
-        for _ in range((max_width + 3) * len(row) + 1):
+        for _ in range(sum(max_widths.values()) + 3 * len(row) + 1):
             print("-", end="")
         print()
 
@@ -49,19 +54,22 @@ try:
     headers = [header[0] for header in db_cursor.description]
     result = db_cursor.fetchall()
 
-    # Get the length of the longest cell in a row
-    max_width = 0
-    for header in headers:
-        max_width = max(max_width, len(header))
+    # Get the length of the longest cell in a collumn and store it in max_widths
+    max_widths = {}
+    for index, header in enumerate(headers):
+        if index not in max_widths.keys():
+            max_widths[index] = 0
+        max_widths[index] = max(max_widths[index], len(header))
     for row in result:
-        max_width = max(max_width, max(list(map(len, (map(str, row))))))
+        for index, cell in enumerate(row):
+            max_widths[index] = max(max_widths[index], len(str(cell)))
     
     # Format print the collumn headers
-    print_row(max_width, headers, header=True)
+    print_row(max_widths, headers, header=True)
 
     # Format print each row
     for row in result:
-        print_row(max_width, row)
+        print_row(max_widths, row)
 
     db_cursor.close()
 
